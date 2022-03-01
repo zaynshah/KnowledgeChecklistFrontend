8d0fc6e8ddc08480a37fdfefd0e108ff5e277822
FahmidulHaquee <fahmidulhaque1997@gmail.com>
2022-03-01 09:35:57 +0000

    Most of the functionality of the dashboard has been completed.

---
+++ knowledge-challenge-app/src/Components/LO/radios.js
@@ -0,0 +1,20 @@
+export const radios = [
+  {
+    text: "Not confident",
+    score: "4",
+    className: "unconfident",
+    variant: "outline-danger",
+  },
+  {
+    text: "Needs revision",
+    score: "3",
+    className: "neutral",
+    variant: "outline-warning",
+  },
+  {
+    text: "Feel confident",
+    score: "2",
+    className: "confident",
+    variant: "outline-success",
+  },
+];
--- knowledge-challenge-app/src/Components/confidenceButton/confidenceButton.js
+++ knowledge-challenge-app/src/Components/confidenceButton/confidenceButton.js
@@ -1,70 +1,21 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Button from "react-bootstrap/Button";
 
 function ConfidenceButton(props) {
-  const [isActive, setIsActive] = useState(false);
-  const [isClicked, setIsClicked] = useState(false);
-
   function handleSubmit(e) {
-    props.changeRating(e.target.id);
-
-    if (isActive) {
-      props.changeProgress("decrease");
-    } else {
-      props.changeProgress("increase");
-    }
-
-    props.updateScore(props.score);
-
-    if (!isActive) {
-      console.log(props.text + " - Making active");
-      setIsActive(true);
-    } else {
-      console.log(props.text + " - Making inactive");
-      setIsActive(false);
-    }
+    props.updateScore(parseInt(e.target.id));
   }
 
-  function changeButton() {
+  function createButton(score, text, variant) {
     return (
-      <div className="export-pdf-button">
-        <>
-          <style type="text/css">
-            {`
-                    .btn-pdf {
-                      background-color: #d14420;
-                      color: white;
-                      margin-top: 30px;
-                      margin-bottom: 30px;  
-                    }
-                    `}
-          </style>
-          <Button className="export-pdf-button" variant="pdf">
-            Save to PDF
-          </Button>
-        </>{" "}
-      </div>
+      <Button onClick={(e) => handleSubmit(e)} id={score} variant={variant}>
+        {text}
+      </Button>
     );
   }
 
-  function createButton(type, text, variant, isClicked) {
-    if (isClicked) {
-      return (
-        <Button onClick={(e) => handleSubmit(e)} id={type} variant={variant}>
-          I'm clicked
-        </Button>
-      );
-    } else {
-      return (
-        <Button onClick={(e) => handleSubmit(e)} id={type} variant={variant}>
-          {text}
-        </Button>
-      );
-    }
-  }
-
-  return <div>{createButton(props.type, props.text, props.variant)}</div>;
+  return <div>{createButton(props.score, props.text, props.variant)}</div>;
 }
 
 export default ConfidenceButton;
