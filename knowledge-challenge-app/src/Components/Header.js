d194706d74a54ed4931128045a108e1e4c625bbb
Zayn Shah <77988727+zaynshah@users.noreply.github.com>
2022-03-01 13:52:07 +0000

    fixed header and delete request

--- knowledge-challenge-app/src/Components/Header.js
+++ knowledge-challenge-app/src/Components/Header.js
@@ -12,21 +12,29 @@ export default function Header(props) {
             src="https://images.squarespace-cdn.com/content/v1/5ffb1a1492e37d475767ab9a/1612779241752-J2PFODJ7U3QN5NIA3YDI/1.png?format=200w"
           />
         </div>
-        <div className="btn-div">
+        {props.cook ? (
+          <div className="btn-div">
+            <Dropdown>
+              <Dropdown.Toggle variant="success" id="dropdown-basic" className="cc">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-person-fill" viewBox="2 2 16 16">
+                  <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
+                </svg>
+                <span>
+                  Signed in as: <u>{props.cook}</u>
+                </span>
+              </Dropdown.Toggle>
+              <Dropdown.Menu>
+                <Dropdown.Item onClick={props.logOut}>Log Out</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+          </div>
+        ) : (
+          <></>
+        )}
+        {/* <div className="btn-div">
           <Dropdown>
-            <Dropdown.Toggle
-              variant="success"
-              id="dropdown-basic"
-              className="cc"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="currentColor"
-                className="bi bi-person-fill"
-                viewBox="2 2 16 16"
-              >
+            <Dropdown.Toggle variant="success" id="dropdown-basic" className="cc">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-person-fill" viewBox="2 2 16 16">
                 <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
               </svg>
               <span>
@@ -37,7 +45,7 @@ export default function Header(props) {
               <Dropdown.Item onClick={props.logOut}>Log Out</Dropdown.Item>
             </Dropdown.Menu>
           </Dropdown>
-        </div>
+        </div> */}
       </div>
     </div>
   );
